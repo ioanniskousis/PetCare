3bd61ef3024136f2b8baaf4a97ae4b6ee5557f1d
Ioannis Kousis <bonobo@otenet.gr>
2021-04-23 17:30:34 +0300

    final style improvements

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,12 +1,2 @@
 module HomeHelper
-  def homeCaption
-    "Pet Care"
-  end
-
-  def bottomMessage
-    "This application aims for pet owners to track their friends' treatment.<br/> 
-    Users have to login to the application and insert information about any number of pets they own.<br/>
-    They can record any event of their pets’ lives like vaccinations, illnesses, nutrition, grooming, weight control, everyday hobbies etc.<br/>
-    Also, they can schedule appointments with a vet or a groomer."
-  end
 end
