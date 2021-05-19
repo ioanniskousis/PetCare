de94b8e674e6e7e8104eac46dc1b04a66f8daa43
Ioannis Kousis <bonobo@otenet.gr>
2021-05-19 17:10:17 +0300

    implement tips on mouse hover

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,48 +8,58 @@ module ApplicationHelper
   end
 
   def linkToRoot
-    link_to(image_tag('home-image.png', class: 'top-bar-link-img'), root_path)
+    tip = content_tag(:div, "Home", :class => "tiptext left bottom")
+    link = link_to(image_tag('home-image.png', class: 'top-bar-link-img') + tip, root_path, class: 'tipped')
+
+    link
   end
 
   def linkToDashboard
     if current_user
-      link_to(image_tag('dashboard.png', class: 'top-bar-link-img-25'), dashboard_path)
+      tip = content_tag(:div, "Dashboard", :class => "tiptext bottom")
+      link_to(image_tag('dashboard.png', class: 'top-bar-link-img-25') + tip, dashboard_path, class: 'tipped')
     end
   end
 
   def linkToUsers
     if current_user
-      link_to(image_tag('users.png', class: 'top-bar-link-img-25'), users_path, method: :get)
+      tip = content_tag(:div, "Users", :class => "tiptext bottom")
+      link_to(image_tag('users.png', class: 'top-bar-link-img-25') + tip, users_path, class: 'tipped', method: :get)
     end
   end
 
   def startChatButton
     if current_user
-      button_tag(image_tag('chat.png', class: 'top-bar-link-img-25'), id: 'startChatButton', onClick: 'chat_clicked()')
+      tip = content_tag(:div, "Chat", :class => "tiptext bottom")
+      button_tag(image_tag('chat.png', class: 'top-bar-link-img-25') + tip, id: 'startChatButton', class: 'tipped', onClick: 'chat_clicked()')
     end
   end
 
   def linkToLogIn
     if !current_user && (controller_name != 'sessions')
-      link_to(image_tag('login.png', class: 'top-bar-link-img-25'), new_session_path(:user))
+      tip = content_tag(:div, "Log In", :class => "tiptext bottom right")
+      link_to(image_tag('login.png', class: 'top-bar-link-img-25') + tip, new_session_path(:user), class: 'tipped')
     end
   end
 
   def linkToSignUp
-    if !current_user && (controller_name != 'registrations') 
-      link_to(image_tag('signup.png', class: 'top-bar-link-img-25'), new_registration_path(:user))
+    if !current_user && (controller_name != 'registrations')
+      tip = content_tag(:div, "Sign Up", :class => "tiptext right bottom")
+      link_to(image_tag('signup.png', class: 'top-bar-link-img-25') + tip, new_registration_path(:user), class: 'tipped')
     end
   end
 
   def linkToForgot
-    if controller_name == 'sessions' 
-      link_to(image_tag('forgot.png', class: 'top-bar-link-img-25'), new_password_path(:user))
+    if controller_name == 'sessions'
+      tip = content_tag(:div, "Forgot password", :class => "tiptext bottom")
+      link_to(image_tag('forgot.png', class: 'top-bar-link-img-25') + tip, new_password_path(:user), class: 'tipped')
     end
   end
 
   def logoutButton
     if current_user
-      link_to(image_tag('logout.png', class: 'top-bar-link-img-25'), destroy_user_session_path, method: :delete)
+      tip = content_tag(:div, "Log Out", :class => "tiptext right bottom")
+      link_to(image_tag('logout.png', class: 'top-bar-link-img-25') + tip, destroy_user_session_path, class: 'tipped', method: :delete)
     end
   end
 
