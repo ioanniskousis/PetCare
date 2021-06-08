da92e71f7d443644d57743025bd84e13ae7d15d8
Ioannis Kousis <bonobo@otenet.gr>
2021-06-08 10:20:37 +0300

    added jquery

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -3,6 +3,7 @@ import consumer from "./consumer"
 consumer.subscriptions.create("ChatRoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log("Connected to the chat room!");
   },
 
   disconnected() {
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'chat/index'
   resources :found_photos
   resources :founds
   resources :lost_photos
